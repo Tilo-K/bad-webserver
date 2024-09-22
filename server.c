00c37439c219823445351d013b4afda4c87b39c2
Tilo-K <tilo@klarenbeek.de>
2024-09-22 21:29:58 +0200

    feat: add socket listening

--- server.c
+++ server.c
@@ -1,7 +1,22 @@
 #include "sc/socket/sc_sock.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-void run_app() {}
+void run_app() {
+  char *host = "0.0.0.0";
+  char *port = "8080";
+
+  struct sc_sock *socket = malloc(sizeof(struct sc_sock));
+  sc_sock_init(socket, 0, true, SC_SOCK_INET);
+
+  int error = sc_sock_listen(socket, host, port);
+  if (error != 0) {
+    puts(sc_sock_error(socket));
+  }
+
+  sc_sock_term(socket);
+  free(socket);
+}
 
 int main(int argn, char **argv) {
 
