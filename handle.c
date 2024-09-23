1e355ff03c99ba8a5b8c2e24891aa955e77c0f69
Tilo-K <tilo@klarenbeek.de>
2024-09-23 20:58:32 +0200

    feat: thread creation

--- handle.c
+++ handle.c
@@ -1,3 +1,13 @@
+#include "sc/mutex/sc_mutex.h"
+#include "sc/queue/sc_queue.h"
 #include "sc/socket/sc_sock.h"
 
-void handle_client(struct sc_sock *client_sock) { return; }
+struct HandleClientArgs {
+  struct sc_queue_ptr *queue;
+  struct sc_mutex *mutex;
+};
+
+void *handle_client(void *args) {
+  struct HandleClientArgs *cargs = args;
+  return NULL;
+}
--- server.c
+++ server.c
@@ -1,4 +1,8 @@
+#include "handle.c"
+#include "sc/mutex/sc_mutex.h"
+#include "sc/queue/sc_queue.h"
 #include "sc/socket/sc_sock.h"
+#include "sc/thread/sc_thread.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,8 +10,22 @@ void run_app() {
   char *host = "0.0.0.0";
   char *port = "8080";
 
+  struct sc_queue_ptr conn_queue;
+  sc_queue_init(&conn_queue);
+
+  struct sc_mutex queue_mutex;
+  sc_mutex_init(&queue_mutex);
+
   struct sc_sock *socket = malloc(sizeof(struct sc_sock));
   sc_sock_init(socket, 0, true, SC_SOCK_INET);
+
+  struct HandleClientArgs args = {.queue = &conn_queue, .mutex = &queue_mutex};
+
+  struct sc_thread threads[10];
+  for (int i = 0; i < 10; i++) {
+    sc_thread_init(&threads[i]);
+    sc_thread_start(&threads[i], handle_client, &args);
+  }
 
   int error = sc_sock_listen(socket, host, port);
   if (error != 0) {
